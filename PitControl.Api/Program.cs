3d260dd90b68d9b55b92e61c0c1cbdc7419764a7
Higor <higoralmeida7022@gmail.com>
2025-08-25 12:18:41 -0300

    chore(api): configura Swagger e endpoints da API
    -Adicionadado configuração do Swagger no builder.Services

--- PitControl.Api/Program.cs
+++ PitControl.Api/Program.cs
@@ -1,22 +1,34 @@
 using PitControl.Infra.Ioc;
-
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
+builder.Services.AddControllers();
 builder.Services.AddInfrastructure(builder.Configuration);
+
+// Swagger (Swashbuckle)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PitControl API", Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PitControl API v1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
