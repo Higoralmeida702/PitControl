7ca01a1400cd39459fa0488f4868fc6033362c7c
Higor <higoralmeida7022@gmail.com>
2025-09-16 12:34:30 -0300

    fix(produto): corrigido propriedades do ProdutoDto para serem públicas

--- PitControl.Application/Dto/ProdutoDto.cs
+++ PitControl.Application/Dto/ProdutoDto.cs
@@ -7,27 +7,25 @@ namespace PitControl.Application.Dto
     public class ProdutoDto
     {
         [Required(ErrorMessage = "É necessário o preenchimento do campo informando o nome da peça")]
-        public string NomePeca { get; private set; }
+        public string NomePeca { get;  set; }
 
         [Required(ErrorMessage = "É necessário o preenchimento do campo informando o fabricante da peça")]
-        public string Fabricante { get; private set; }
+        public string Fabricante { get;  set; }
 
         [Required(ErrorMessage = "É necessário o preenchimento do campo informando a localização da peça no estoque")]
-        public string LocalizacaoEstoque { get; private set; }
+        public string LocalizacaoEstoque { get;  set; }
 
         [Required(ErrorMessage = "É necessário o preenchimento do campo informando o peso da peça")]
-        public decimal Peso { get; private set; }
+        public decimal Peso { get;  set; }
 
         [Required(ErrorMessage = "É necessário o preenchimento do campo informando a altura da peça")]
-        public decimal Altura { get; private set; }
+        public decimal Altura { get;  set; }
 
         [Required(ErrorMessage = "É necessário o preenchimento do campo informando a largura da peça")]
-        public decimal Largura { get; private set; }
+        public decimal Largura { get;  set; }
 
         [Required(ErrorMessage = "É necessário o preenchimento do campo informando o comprimento da peça")]
-        public decimal Comprimento { get; private set; }
-
-        public Fornecedor Fornecedor { get; set; }
+        public decimal Comprimento { get;  set; }
 
         [Required(ErrorMessage = "É necessário o preenchimento do campo informando o fornecedor da peça")]
         public int FornecedorId { get; set; }
