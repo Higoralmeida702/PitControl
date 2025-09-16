cbc7fe1a030cd2bd4ce90a979db0e3721e59dc9c
Higor <higoralmeida7022@gmail.com>
2025-09-16 15:43:44 -0300

    feat(setor-estoque): adicionado propriedades NomeSetor, Corredor  e Posicao com validação de domínio

--- PitControl.Domain/Model/SetorEstoque.cs
+++ PitControl.Domain/Model/SetorEstoque.cs
@@ -5,14 +5,32 @@ namespace PitControl.Domain.Model
 {
     public class SetorEstoque
     {
-        public int Id { get; set; }
-        public string NomeSetor { get; set; }
+        public int Id { get; private set; }
+        public string NomeSetor { get; private set; }
+        public string Corredor { get; private set; }
+        public string Posicao { get; private set; }
         public ICollection<ProdutoEmEstoque> ProdutosEmEstoque { get; set; } = new List<ProdutoEmEstoque>();
 
-        public SetorEstoque(string nomeSetor)
+
+        public SetorEstoque(string nomeSetor, string corredor, string posicao)
+        {
+            ValidateDomain(nomeSetor, corredor, posicao);
+        }
+
+        private void ValidateDomain(string nomeSetor, string corredor, string posicao)
         {
             DomainExceptionValidation.When(string.IsNullOrEmpty(nomeSetor), "Nome do setor é obrigatório.");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(corredor), "Corredor é obrigatório.");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(posicao), "Posição é obrigatória.");
+
             NomeSetor = nomeSetor;
+            Corredor = corredor;
+            Posicao = posicao;
+        }
+
+        public void Update(string nomeSetor, string corredor, string posicao)
+        {
+            ValidateDomain(nomeSetor, corredor, posicao);
         }
     }
 }
